0a997621b44f1b0ee5134fe0c67ac429265c98e5
Jason Buckley <jbuckley@csumb.edu>
2021-03-15 21:15:12 -0700

    updated dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
+const { json } = require('express');
 var express = require('express');
+const db = require('../middleware/db')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rubenson' });
 });
+
+router.get('/add-mockdata', async function(req, res){
+  //Sql line: INSERT INTO Student VALUES(NULL, "email@email.com", "first1", "m1", "last1", "8318318310", NULL, NULL, NULL, 11, "current")
+  let query = "INSERT INTO Student VALUES(NULL,?,?,?,?,?,NULL,NULL,NULL,?,?)"
+  //to add another mockdata point copy the sample values and replace them with your values
+  // sample values: ["email@email.com", "first1", "m1", "last1", "8318318310", 11, "current"]
+  let mockdata = [
+    ["email@email.com", "first1", "m1", "last1", "8318318310", 11, "current"],
+    ["jbuckley@csumb.edu", "John", "middle", "Smith", "8318318311", 13, "Former"]
+  ]
+
+  for(n in mockdata){
+    // checks if the row is already in the db
+    let info = await db.query(db.pool, "SELECT * FROM Student WHERE student_email=?", mockdata[n][0]).catch((err) => {
+      return []
+    });
+
+    // if the mockdata row is not in the db insert it
+    if(Array.isArray(info) && !info.length){
+      await db.query(db.pool, query, mockdata[n]).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
+  return res.json({success: true});
+})
 
 module.exports = router;
